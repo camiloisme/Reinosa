3a5d07e2ffd6df093062cccb1ca4f58528bc07a3
Camilo <camiloesyo@gmail.com>
2025-07-26 13:52:44 +0200

    Intento 3 de menu hamburguesa

--- menu.js
+++ menu.js
@@ -1,6 +1,13 @@
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.querySelector(".nav-link");
 
-hamburger.addEventListener("click", () =>{
-    navLinks.classList.toggle("active");
-});
+  const abrirBtn = document.getElementById('abrir');
+  const cerrarBtn = document.getElementById('cerrar');
+  const navLinks = document.querySelector('.nav-link');
+
+  abrirBtn.addEventListener('click', () => {
+    navLinks.classList.add('visible');
+  });
+
+  cerrarBtn.addEventListener('click', () => {
+    navLinks.classList.remove('visible');
+  });
+

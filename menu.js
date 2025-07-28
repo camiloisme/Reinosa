aa8938f332362361067e70468ac7d51584031177
Camilo <camiloesyo@gmail.com>
2025-07-28 16:48:12 +0200

    Cookie lista

--- menu.js
+++ menu.js
@@ -8,3 +8,30 @@ abrir.addEventListener ("click", () => {
 cerrar.addEventListener ("click", () =>{
     nav.classList.remove("visible");
 })
+
+const banner = document.querySelector('#cookies-id');
+const aceptar = document.querySelector('#aceptar-id');
+const rechazar = document.querySelector('#rechazar-id');
+
+/*Quitar el banner si acepta o no*/
+
+window.onload = () => {
+    const cookieDecision = localStorage.getItem('cookieConsent');
+    if (cookieDecision === 'accepted' || cookieDecision === 'rejected'){
+        banner.style.display = 'none';
+    }
+};
+
+/* Si click acepta */
+ 
+aceptar.addEventListener('click', () => {
+    localStorage.setItem('cookieConsent', 'accepted');
+    banner.style.display = 'none';
+});
+
+/* Si click rechaza */
+rechazar.addEventListener('click', () => {
+    localStorage.setItem('cookieConsent', 'rejected');
+    banner.style.display = 'none';
+
+})
